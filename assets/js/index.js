da8e80314dd11b97f0892dd480125329e05612c1
SachinJaiswal2020 <sachin.jayswal2020@gmail.com>
2021-06-06 18:52:06 +0530

    Scroll on Top and some changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,5 +31,17 @@ $(document).ready(function(){
 
    
 
-
+// Scroll to TOP
+var btn = $('.scrollup');
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 500) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '500');
+});
 
